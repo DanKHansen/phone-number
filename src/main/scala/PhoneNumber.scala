0b5f6974a73088c9c447abaa376176e15bd026a0
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-17 14:33:03 +0100

    pure regex one-liner

--- src/main/scala/PhoneNumber.scala
+++ src/main/scala/PhoneNumber.scala
@@ -1,11 +1,3 @@
 object PhoneNumber:
    def clean(s: String): Option[String] =
-      val onlyDigits = s.filter(_.isDigit)
-      if s.length < 10 || onlyDigits.length < 10 || onlyDigits.length > 11 then None
-      else
-         onlyDigits.length match
-            case 10 if List('0', '1').contains(onlyDigits.head) => None
-            case 10 if List('0', '1').contains(onlyDigits(3))   => None
-            case 10                                             => Some(onlyDigits)
-            case 11 if onlyDigits.head == '1'                   => Some(onlyDigits.tail)
-            case _                                              => None
+      """(?<=^1?)[2-9]\d{2}[2-9]\d{6}$""".r.findFirstIn(s.filter(_.isDigit))
