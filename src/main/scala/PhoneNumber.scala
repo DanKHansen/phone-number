94b1e88656abdfec3aaeaa73e00922b6b5322e2e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-17 14:35:31 +0100

    refactoring

--- src/main/scala/PhoneNumber.scala
+++ src/main/scala/PhoneNumber.scala
@@ -1,3 +1,3 @@
 object PhoneNumber:
    def clean(s: String): Option[String] =
-      """(?<=^1?)[2-9]\d{2}[2-9]\d{6}$""".r.findFirstIn(s.filter(_.isDigit))
+      "(?<=^1?)[2-9]\\d{2}[2-9]\\d{6}$".r.findFirstIn(s.filter(_.isDigit))
